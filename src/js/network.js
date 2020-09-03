1e688e2aa4089801feb68987eb0a1fc4f6139453
Andrei Vettor <andreivettor@gmail.com>
2020-09-03 23:39:50 +0300

    Reverted to control theory core architecture and added sliders

--- src/js/network.js
+++ src/js/network.js
@@ -1,38 +1,92 @@
-var labels = [
-    "Needs",
-    "Financial",
-    "Satisfaction"
-]
-
-var weights = [
-    [   0, -0.2, -0.2],
-    [-0.3,    0, +0.3],
-    [   0, +0.2,    0]
-];
-
-var focus = [
-    0,
-    10,
-    0
-];
-
-var htmlObjects = [];
+const simulationStepTime = 500;
 const subjectList = document.getElementsByClassName('subjectList')[0];
 
-const simulationStepTime = 200;
-const act_threshold = 0.5;
-const weight_change_rate = 1.10;
-const weight_decay_rate = 0.05;
+var subjects = [];
+var htmlObjects = [];
 
-// Zero filled matrices
-var activations = Array(focus.length).fill(0);
-var newActivations = Array(focus.length).fill(0);
+subjects.push({
+    label: "Needs",
+    value: 20,
+    focus: 0.5,
+    compute: function() {
+        this.value = 
+            -this.focus 
+            - 0.1 * getVal("Health") 
+            + 0.1 * getVal("Career") 
+            + this.feedback() 
+            + getVariance();
+    },
+    feedback: function() {
+        return 0.95 * this.value;
+    }
+});
 
-for(var i = 0; i < focus.length; ++i)
+subjects.push({
+    label: "Health",
+    value: 80,
+    focus: 0.3,
+    compute: function() {
+        this.value = 
+            this.focus 
+            + 0.1 * getVal("Needs") 
+            + this.feedback() 
+            + getVariance();
+    },
+    feedback: function() {
+        return 0.95 * this.value;
+    }
+});
+
+subjects.push({
+    label: "Career",
+    value: 80,
+    focus: 0.2,
+    compute: function() {
+        this.value = 
+            this.focus 
+            + 0.1 * getVal("Health")
+            - 0.1 * getVal("Needs")
+            + this.feedback() 
+            + getVariance();
+    },
+    feedback: function() {
+        return 0.95 * this.value;
+    }
+});
+
+function getVal(label) {
+    return subjects.find(subject => subject.label == label).value;
+}
+
+function getVariance() {
+    return Math.random();
+}
+
+// Procedurally generate each subjects HTML
+for(var i = 0; i < subjects.length; ++i)
 {
-    htmlObjects[i] = {};
     htmlObjects[i] = document.createElement('div');
     htmlObjects[i].setAttribute('class', 'subject');
+
+    let labelSpan = document.createElement('span')
+    labelSpan.setAttribute('class','label');
+    labelSpan.innerText = subjects[i].label;
+    htmlObjects[i].appendChild(labelSpan);
+
+    let valueSpan = document.createElement('span');
+    valueSpan.setAttribute('class','value')
+    valueSpan.innerText = subjects[i].value;
+    htmlObjects[i].appendChild(valueSpan);
+
+    let slider = document.createElement('input');
+    slider.setAttribute('class','slider');
+    slider.setAttribute('type','range');
+    slider.setAttribute('min','0');
+    slider.setAttribute('max','1');
+    slider.setAttribute('step','0.01');
+    slider.value = subjects[i].focus;
+
+    htmlObjects[i].appendChild(slider);
     subjectList.appendChild(htmlObjects[i]);
 }
 
@@ -43,45 +97,15 @@ setInterval(function(){
 
 // Tick function
 function simulationStep()
-{
-    // Update activations
-    for(var i = 0; i < focus.length; ++i) {
-        // Level starts from focus/bias
-        var level = focus[i];
-
-        // Add the weighted previous activations
-        for(var j = 0; j < focus.length; ++j) {
-            level += weights[j][i] * activations[j];
-        }
-
-        // Determine activation
-        if(level > act_threshold) {
-            newActivations[i] = 1;
-        }
-        else {
-            newActivations[i] = 0;
-        }
-    }
-    // Transfer new activation values for next step
-    activations = [...newActivations];
-
-    // Update weights
-    for(var i = 0; i < focus.length; ++i) {
-        for(var j = 0; j < focus.length; ++j) {
-            if(newActivations[i] == 1) {
-                weights[i][j] *= weight_change_rate;
-            }
-            weights[i][j] *= (1 - weight_decay_rate);
-        }
+{   
+    // Update focus values
+    for (let i = 0; i < subjects.length; i++) {
+        subjects[i].focus = parseFloat(htmlObjects[i].getElementsByTagName('input')[0].value);
     }
 
-    // Update visuals
-    for(var i = 0; i < focus.length; ++i)
-    {
-        htmlObjects[i].innerText = labels[i];
-        for(var j = 0; j < focus.length; ++j)
-        {
-            htmlObjects[i].innerText += " " + weights[i][j].toFixed(3);
-        }
+    // Update subject values and draw
+    for (let i = 0; i < subjects.length; i++) {
+        subjects[i].compute();
+        htmlObjects[i].getElementsByClassName('value')[0].innerText = subjects[i].value.toFixed(2);
     }
 }
